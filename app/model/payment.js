74450047e11909fdce8195cf834c18c618d045e2
kgunasekaren <kgunasekaren@ebay.com>
2015-12-28 01:35:24 -0800

    Initial Commit

--- app/model/payment.js
+++ app/model/payment.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 // define our nerd model
 // module.exports allows us to pass this to other files when it is called
-module.exports = mongoose.model('Nerd', {
+module.exports = mongoose.model('Payment', {
     name : {type : String, default: ''}
 });
--- routes.js
+++ routes.js
@@ -7,16 +7,16 @@ module.exports = function(app) {
     // authentication routes
 
     // sample api route
-    app.get('/api/nerds', function(req, res) {
+    app.get('/api/paymenthistory', function(req, res) {
         // use mongoose to get all nerds in the database
-        Nerd.find(function(err, nerds) {
+        PaymentHistory.find(function(err, payment) {
 
             // if there is an error retrieving, send the error.
             // nothing after res.send(err) will execute
             if (err)
                 res.send(err);
 
-            res.json(nerds); // return all nerds in JSON format
+            res.json(payment); // return all nerds in JSON format
         });
     });
 
